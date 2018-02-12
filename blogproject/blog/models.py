8495ab76969439480eee344736237310cb5b1ef0
zaidozaid <zaidozaid@gmail.com>
2018-02-12 17:43:47 +0530

    S

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -21,8 +21,50 @@ class Post(models.Model):
         self.publish_date= timezone.now()
         self.save()
 
-    def approve_coments(self):
-        return self.comments.f
+    def approve_comments(self):
+        return self.comments.filter(approved_comment = True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
+
+
+    def __str__(self):
+
+        return self.title
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignObject('blog.Post',related_name='comments')
+    author = models.CharField(max_length=254)
+    text = models.TextField()
+    create_date = models.DateField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+
+    def __str__(self):
+        return self.text
+
+
+
+
+
+
+
+
+
 
 
 
